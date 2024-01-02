f4a402344f1791a99a716f9b82e878ad6605dc72
KBeKind <kyle@kbekind.com>
2024-01-02 10:03:12 -0600

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-
+use clap::Parser;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use std::collections::{VecDeque, LinkedList};
-use clap::Parser;
 use rust_proj_8::create_fruit_salad;
+use std::collections::{LinkedList, VecDeque};
 
 use std::collections::HashMap;
 
-use petgraph::graph::{NodeIndex, UnGraph, self, Node};
+use petgraph::graph::{self, Node, NodeIndex, UnGraph};
 use petgraph::Direction;
 use std::fmt::{self, write};
+
+use std::collections::HashSet;
+
+use std::collections::BTreeSet;
+
+fn generate_fruit() -> &'static str {
+    let fruits: [&str; 8] = [
+        "banana", "apple", "orange", "grapes", "cherry", "fig", "berry", "bean",
+    ];
+    let mut rng = rand::thread_rng();
+    fruits.choose(&mut rng).unwrap()
+}
 
 // #[derive(Parser)]
 // #[clap(version = "3.0", author = "me", about = "number of fruits to include in salad")]
@@ -36,17 +47,14 @@ impl fmt::Display for Fighter {
     }
 }
 
-fn add_edge(graph: &mut UnGraph<&Fighter, f32>, nodes: &[NodeIndex], a: usize, b:usize) {
+fn add_edge(graph: &mut UnGraph<&Fighter, f32>, nodes: &[NodeIndex], a: usize, b: usize) {
     graph.add_edge(nodes[a], nodes[b], 1.0);
 }
 
 fn main() {
+    // DATA STRUCTURES
 
-
-    
-  // DATA STRUCTURES
-
-  // A VECTOR IS A GROWABLE ARRAY
+    // A VECTOR IS A GROWABLE ARRAY
     let mut fruit: Vec<&str> = vec!["banana", "apple", "orange", "grapes", "cherry", "fig"];
 
     // SCRAMBLE/SHUFFLE THE FRUIT
@@ -55,16 +63,14 @@ fn main() {
 
     println!("Fruit Salad");
     for (i, item) in fruit.iter().enumerate() {
-        
         if i != fruit.len() - 1 {
             print!("{}, ", item);
         } else {
             println!("{}", item);
         }
-    };
-    
-    // VECDEQUE IS A DOUBLE ENDED QUEUE, IT MEANS YOU CAN PUSH AND POP FROM BOTH ENDS
+    }
 
+    // VECDEQUE IS A DOUBLE ENDED QUEUE, IT MEANS YOU CAN PUSH AND POP FROM BOTH ENDS
 
     let mut salad: VecDeque<&str> = VecDeque::new();
 
@@ -77,21 +83,18 @@ fn main() {
     let mut salad: Vec<_> = salad.into_iter().collect();
     fruit.shuffle(&mut rng2);
 
-    
     // CONVERT IT BACK TO VecDeque
     let mut salad: VecDeque<_> = salad.into_iter().collect();
     salad.push_front("afruitagain");
 
     println!("Fruit Salad2");
     for (i, item) in salad.iter().enumerate() {
-        
         if i != salad.len() - 1 {
             print!("{}, ", item);
         } else {
             println!("{}", item);
         }
-    };
-
+    }
 
     // LINKED LIST
     // ALLOWS INSERTION OR DELETION FROM THE MIDDLE OF THE LIST
@@ -102,7 +105,6 @@ fn main() {
     stuff.push_back("Thing2");
     stuff.push_back("Thing3");
 
-
     stuff.push_back("thing4");
 
     // GET NUMBER OF FRUITS USER REQUESTED
@@ -112,12 +114,13 @@ fn main() {
 
     // PRINT THE SALAD
 
-
     let numbers: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 3];
     let result: Vec<(i32, u32)> = logic(numbers);
 
-    println!("the frequencty of each number in the vector is {:?}", result);
-
+    println!(
+        "the frequencty of each number in the vector is {:?}",
+        result
+    );
 
     let mut languages: HashMap<String, i32> = init_languages();
     let weights = calculate_weights(&mut languages);
@@ -126,7 +129,6 @@ fn main() {
     for (language, weight) in &weights {
         println!("{}: {}", language, weight);
     }
-
 
     let mut graph = UnGraph::new_undirected();
 
@@ -138,11 +140,10 @@ fn main() {
         Fighter::new("Mike"),
     ];
 
-
     let fighter_nodes: Vec<NodeIndex> = fighers
-    .iter()
-    .map(|fighter | graph.add_node(fighter))
-    .collect();
+        .iter()
+        .map(|fighter| graph.add_node(fighter))
+        .collect();
 
     add_edge(&mut graph, &fighter_nodes, 0, 1);
     add_edge(&mut graph, &fighter_nodes, 0, 2);
@@ -154,25 +155,53 @@ fn main() {
     add_edge(&mut graph, &fighter_nodes, 2, 4);
 
     for (i, &node) in fighter_nodes.iter().enumerate() {
-       let name = &fighers[i].name;
-       let degree = graph.edges_directed(node, Direction::Outgoing).count() as f32;
-       let closeness = 1.0 / degree;
-       println!("The closeness centrality of {} is {:.2}", name, closeness);
+        let name = &fighers[i].name;
+        let degree = graph.edges_directed(node, Direction::Outgoing).count() as f32;
+        let closeness = 1.0 / degree;
+        println!("The closeness centrality of {} is {:.2}", name, closeness);
 
-       match name.as_str() {
-           "John" => println!("John is the best {}, {}", name, closeness),
-           "Jane" => println!("Jane is the best {}, {}", name, closeness),
-           "Bob" => println!("Bob is the best {}, {}", name, closeness) ,
-           "Alice" => println!("Alice is the best {}, {}", name, closeness),
-           "Mike" => println!("Mike is the best {}, {}", name, closeness),
-           _ => println!("No one is the best {}, {}", name, closeness),
-       }
-       println!("----------------------");
+        match name.as_str() {
+            "John" => println!("John is the best {}, {}", name, closeness),
+            "Jane" => println!("Jane is the best {}, {}", name, closeness),
+            "Bob" => println!("Bob is the best {}, {}", name, closeness),
+            "Alice" => println!("Alice is the best {}, {}", name, closeness),
+            "Mike" => println!("Mike is the best {}, {}", name, closeness),
+            _ => println!("No one is the best {}, {}", name, closeness),
+        }
+        println!("----------------------");
+    }
+
+    let mut fruit_set = HashSet::new();
+    println!("Gen 100 random fruits...");
+    for _ in 0..100 {
+        fruit_set.insert(generate_fruit());
+    }
+    println!("Num of unique fruits gen: {}", fruit_set.len());
+
+    let fruits_list: Vec<&str> = vec![
+        "banana", "apple", "orange", "grapes", "cherry", "fig", "berry", "bean", "mushroom",
+    ];
+
+    let amounts = [1, 3, 5, 7, 9];
+
+    let mut rng = thread_rng();
+
+    for amount in amounts.iter() {
+        let mut fruits_set = BTreeSet::new();
+        let mut shuffled_fruits = fruits_list.clone();
+        shuffled_fruits.shuffle(&mut rng);
+
+        for a_fruit in shuffled_fruits {
+            fruits_set.insert(a_fruit);
+            if fruits_set.len() >= *amount {
+                break;
+            }
+        }
+        println!("{}: {:?}", amount, fruits_set);
     }
 }
 
 fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
-    
     // HASHMAP
     // HASHMAP HAS GREAT PERFORMANCE
     let mut frequencies: HashMap<i32, u32> = HashMap::new();
@@ -188,9 +217,7 @@ fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
     }
 
     result
-
 }
-
 
 // MORE WITH HASMAPS
 fn init_languages() -> HashMap<String, i32> {
